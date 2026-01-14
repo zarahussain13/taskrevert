2e5cd18f2e4a9dab3904d741ec1836b42a947f56
zarahussain13 <azarahussain098.com@gmail.com>
2026-01-14 19:12:06 +0500

    first check

---
+++ code.js
@@ -0,0 +1,8 @@
+document.getElementById("cta-btn").addEventListener("click", () => {
+  alert("Welcome! Let's get started 🚀");
+});
+
+function showMessage() {
+  document.getElementById("message").innerText =
+    "Thanks for reaching out! We'll contact you soon.";
+}
