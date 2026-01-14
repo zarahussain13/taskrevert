d3f17042da3b5c0ccee3d63dd38bdc51e1e08bf0
zarahussain13 <azarahussain098.com@gmail.com>
2026-01-14 19:20:30 +0500

    Revert "first check"
    This reverts commit 2e5cd18f2e4a9dab3904d741ec1836b42a947f56.

--- code.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("cta-btn").addEventListener("click", () => {
-  alert("Welcome! Let's get started 🚀");
-});
-
-function showMessage() {
-  document.getElementById("message").innerText =
-    "Thanks for reaching out! We'll contact you soon.";
-}
